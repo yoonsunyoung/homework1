748e35f49d7f50efc0d06504ac5cd8c7d40a0454
sunyoung <yoonsunyoung@naver.com>
2018-01-09 21:08:27 +0900

    completed menu

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,8 +1,27 @@
+import java.util.Scanner;
 
 public class Calculator {
+	public static int menu(){
+		System.out.println("[ ID: 1413911 ]");
+		System.out.println("[ Name: À±¼±¿µ   ]");
+		
+		System.out.println("1. Add two numbers");
+		System.out.println("2. Subtract two numbers");
+		System.out.println("3. Multiply two numbers");
+		System.out.println("4. Quit");
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int num;
+		num = scan.nextInt();
+		
+		return num;
+	}
 
 	public static void main(String[] args) {
-
+		int menuNum;
+		
+		menuNum = menu();
 	}
 
 }
